46ddc5cb470af57d8d216c17c55271d84aabbebc
jason@example.com <you@example.com>
2023-12-04 20:23:55 -0500

    my change

---
+++ src/MainAppliction.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+// MainApplication.java
+public class MainApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Payroll payroll = new Payroll();
+
+
+        // Prompt the user to add employees
+        char addMore;
+        do {
+            System.out.println("Enter employee information:");
+            System.out.print("Employee ID: ");
+            int employeeId = scanner.nextInt();
+            scanner.nextLine();  // Consume the newline character
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            System.out.print("Hourly Rate: ");
+            double hourlyRate = scanner.nextDouble();
+            System.out.print("Hours Worked: ");
+            double hoursWorked = scanner.nextDouble();
+
+
+            Employee newEmployee = new Employee(employeeId, name, hourlyRate, hoursWorked);
+            payroll.addEmployee(newEmployee);
+
+
+            System.out.print("Do you want to add another employee? (y/n): ");
+            addMore = scanner.next().charAt(0);
+        } while (addMore == 'y' || addMore == 'Y');
+
+
+        // Display employee details and total payroll
+        payroll.displayEmployeeDetails();
+        System.out.println("--------------------------");
+        System.out.println("Total Payroll: $" + payroll.calculateTotalPayroll());
+
+
+        scanner.close();
+    }
+}
+
