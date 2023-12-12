59e3d01a60f0c3c93762ad32865d817d5105f772
hiteshpatel <hpatel14@umbc.edu>
2023-12-11 20:25:05 -0500

    Added a fail safe on line 30

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
-// MainApplication.java
 public class MainApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Payroll payroll = new Payroll();
         java.util.Date date = new java.util.Date();
 
-        // Prompt the user to add employees
         char addMore;
         do {
-            System.out.println("Enter employee information:");
-            System.out.print("Employee ID: ");
-            int employeeId = scanner.nextInt();
-            scanner.nextLine();  // Consume the newline character
-            System.out.print("Name: ");
-            String name = scanner.nextLine();
-            System.out.print("Hourly Rate: ");
-            double hourlyRate = scanner.nextDouble();
-            System.out.print("Hours Worked: ");
-            double hoursWorked = scanner.nextDouble();
+            try {
+                System.out.println("Enter employee information:");
+                System.out.print("Employee ID: ");
+                int employeeId = scanner.nextInt();
+                scanner.nextLine();  // Consume the newline character
+                System.out.print("Name: ");
+                String name = scanner.nextLine();
+                System.out.print("Hourly Rate: ");
+                double hourlyRate = scanner.nextDouble();
+                System.out.print("Hours Worked: ");
+                double hoursWorked = scanner.nextDouble();
 
-            Employee newEmployee = new Employee(employeeId, name, hourlyRate, hoursWorked);
-            payroll.addEmployee(newEmployee);
+                Employee newEmployee = new Employee(employeeId, name, hourlyRate, hoursWorked);
+                payroll.addEmployee(newEmployee);
 
-            System.out.print("Do you want to add another employee? (y/n): ");
-            addMore = scanner.next().charAt(0);
+                System.out.print("Do you want to add another employee? (y/n): ");
+                addMore = scanner.next().charAt(0);
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter valid information.");
+                scanner.nextLine();  // Consume the invalid input
+                addMore = 'y';  // Set to 'y' to re-enter employee information
+            }
         } while (addMore == 'y' || addMore == 'Y');
 
-        // Display employee details and total payroll
         System.out.println("Payroll for Today's Date: " + date);
         System.out.println("--------------------------");
         payroll.displayEmployeeDetails();
